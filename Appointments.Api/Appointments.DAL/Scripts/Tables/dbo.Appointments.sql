bdbfeec63922c6f9bb503aa9fc9279958e0f42da
shreemadaexploring <shreemada@gmail.com>
2020-12-09 20:09:10 +0530

    added remaining files

---
+++ Appointments.Api/Appointments.DAL/Scripts/Tables/dbo.Appointments.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Appointments]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[dlId] INT NOT NULL,
+	[templateId] int not null,
+	[start] datetime NOT NULL,
+	[end] datetime NOT NULL,
+	[subject] nvarchar(200) NOT NULL,
+	[message] nvarchar(200) NOT NULL,
+)
